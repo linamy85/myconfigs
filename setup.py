4efe1f405ef7d8c63610289cb1c7eccda0b05348
Amy Lin <b03902055@ntu.edu.tw>
2016-11-17 15:11:33 +0800

    create auto script to setup environment
    After setup so many machines...

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cp -r .* ~
+
+# install vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+vim +PluginInstall +qall
+
+# install YouCompletMe
+cd ~/.vim/bundle/YouCompleteMe
+
+./install.py --clang-completer
