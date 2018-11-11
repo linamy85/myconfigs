be02dae29f39271eaf418c18f6635c01c9cb79a7
Amy Lin <linamy85@gmail.com>
2018-11-10 22:04:47 -0800

    Setup script fix error

--- setup.sh
+++ setup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
-rm -rf .git/
+set -e
 
-cd ..
+mkdir -p ~/.vim
 
-mv -r myconfigs/.* ~
+cp -rf .vim/colors ~/.vim
+cp .vimrc ~
+cp .ycm_extra_conf.py ~
+
+cp .bashrc ~
+cp .gitconfig ~
+cp .tmux.conf ~
 
 # install vim plugins
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+BUNDLE=$HOME/.vim/bundle/Vundle.vim
+
+if [ ! -d $BUNDLE ]; then
+  echo "FILE not found"
+  git clone https://github.com/VundleVim/Vundle.vim.git $BUNDLE
+fi
 
 vim +PluginInstall +qall
 
 # install YouCompletMe
 cd ~/.vim/bundle/YouCompleteMe
-
+git submodule update --init --recursive
 ./install.py --clang-completer
